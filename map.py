1c46b657ab05f5878bf1b0c0b697aa1da8d4fc71
Liyuan Menküc <liyuan.menkuec@tu-dortmund.de>
2021-04-08 15:57:51 +0200

    new items

--- map.py
+++ map.py
@@ -12,12 +12,29 @@ Volcano name:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Height: %s m
 """
-map = folium.Map(location=[38, -100], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
 
+def color_producer(elevation):
+    if elevation > 2000:
+        return "green"
+    else:
+        return "blue"
+
+map = folium.Map(location=[38, -100], zoom_start=6, tiles="Stamen Terrain")
+
+#adds Marker 
+fgVolcanoes = folium.FeatureGroup(name="Volcanoes")
 for lati,lonti,ele,name in zip(lat,lon,elev,name):
     iframe = folium.IFrame(html=html % (name, name, ele), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lati,lonti],popup=folium.Popup(iframe), icon=folium.Icon(color='blue')))
+    fgVolcanoes.add_child(folium.Marker(location=[lati,lonti],popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(ele))))
 
-map.add_child(fg)
+#adds GeoJson
+fgBoundary = folium.FeatureGroup(name="Boundary and Population")
+fgBoundary.add_child(folium.GeoJson(data=(open('Webmap_datasources\world.json', 'r', encoding='utf-8-sig').read()), 
+style_function= lambda x: {'fillColor' : 'green' if x['properties']['POP2005'] < 10000000
+else 'red' if 10000000 < x['properties']['POP2005'] < 20000000 else 'orange'}))
+
+#generates map
+map.add_child(fgVolcanoes)
+map.add_child(fgBoundary)
+map.add_child(folium.LayerControl())
 map.save("Map1.html") 
